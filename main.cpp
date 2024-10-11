ff8eba91483fc368a27bc802e72d6ace299c4e54
Abanadja <aabanaba@myuax.com>
2024-10-11 11:48:50 +0200

    Ejercicio 1: Hola mundo

--- main.cpp
+++ main.cpp
@@ -2,8 +2,14 @@
 // Created by alexa on 10/10/2024.
 //
 
-#include "main.h"
+#include <iostream>
+
+int suma(int a, int b) {
+    return a + b;
+}
+
 int main() {
     std::cout << "Hola Mundo" << std::endl;
+    std::cout << "La suma es: " << suma(5, 3) << std::endl;
     return 0;
 }
