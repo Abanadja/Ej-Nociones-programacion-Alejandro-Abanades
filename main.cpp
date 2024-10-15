7fc5f5ec6981e4ea0a82769994d7d770e34f91c5
Abanadja <aabanaba@myuax.com>
2024-10-15 23:36:17 +0200

    Ejercicio 1: Hola mundo

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ int suma(int a, int b) {
 int main() {
     std::cout << "Hola Mundo" << std::endl;
     std::cout << "La suma es: " << suma(5, 3) << std::endl;
-    std::cout << "Valor de g antes de la función suma: " << g << std::endl;
+    std::cout << "Valor de g antes de la funcion suma: " << g << std::endl;
     std::cout << "La suma es: " << suma(5, g) << std::endl;
-    std::cout << "Valor de g después de la función suma: " << g << std::endl;
+    std::cout << "Valor de g despues de la funcion suma: " << g << std::endl;
     return 0;
 }
