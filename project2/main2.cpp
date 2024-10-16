61fb2a7bab1d1d54538042474a74842b30e901be
Abanadja <aabanaba@myuax.com>
2024-10-16 13:25:17 +0200

    Ejercicio 3:desarrollo CMakeLists.txt

---
+++ project2/main2.cpp
@@ -0,0 +1,14 @@
+//
+// Created by alexa on 16/10/2024.
+//
+#include "main2.h"
+#include <iostream>
+#include <fmt/core.h>
+
+int main() {
+  std::cout << "Hola chambales" << std::endl;
+  fmt::print("Hola Fernando Alonso\n");
+  return 0;
+}
+
+
